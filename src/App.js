e6b2083dbf5f98a9c073cd644d7000236697b053
Sahil Patil <sahilpatil0922@gmail.com>
2022-12-02 00:41:33 +0530

    updated files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const apiKey = process.env.REACT_APP_APIKEY
+
+const App = () => {
+  
+  const[progress, setProgress] = useState(0); 
+
+    return (
+      <div>
+        <Router>
+          <LoadingBar
+            height={3}
+            color='#f11946'
+            progress={progress}
+            />
+          <Navbar /> 
+          <Routes>
+              <Route path="/" element ={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={6} country="in" category= "general" />} /> 
+              <Route path="/General" element ={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={6} country="in" category= "general" />} /> 
+              <Route path='/Sports' element={<News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={6} country="in" category= "sports" />} />  
+              <Route path='/Technology' element={<News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={6} country="in" category= "technology" />} />
+              <Route path='/Science' element= {<News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={6} country="in" category= "science" />} /> 
+              <Route path='/Business' element={<News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={6} country="in" category= "business" />} /> 
+              <Route path='/Health' element={ <News setProgress={setProgress} apiKey={apiKey} key="health" pageSize={6} country="in" category= "health" />} />
+              <Route path='/Entertainment' element={<News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={6} country="in" category= "entertainment" />} />   
+              
+            </Routes>
+        </Router>
+      </div>
+    )
 }
 
 export default App;
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import loading from "./Spinner-5.gif";
+
+const Spinner = () => {
+  return (
+    <div className="text-center">
+      <img className="my-3" src={loading} alt="loading" />
+    </div>
+  );
+};
+
+export default Spinner;
